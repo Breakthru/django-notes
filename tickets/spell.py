286a366db8fe218bad3a9fcd88e4e1b7cc7a2ccb
Marco Paladini <marco.paladini@blippar.com>
2017-12-25 00:16:51 +0000

    spelling in js

--- tickets/spell.py
+++ tickets/spell.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import sys
 
-nato_phonetic = {'a': 'Alpha', 'c': 'Charlie', 'b': 'Bravo', 'e': 'Echo', 'd': 'Delta', 'g': 'Golf', 'f': 'Foxtrot', 'i': 'India', 'h': 'Hotel', 'k': 'Kilo', 'j': 'Juliet', 'm': 'Mike', 'l': 'Lima', 'o': 'Oscar', 'n': 'November', 'q': 'Quebec', 'p': 'Papa', 's': 'Sierra', 'r': 'Romeo', 'u': 'Uniform', 't': 'Tango', 'w': 'Whiskey', 'v': 'Victor', 'y': 'Yankee', 'x': 'X-ray', 'z': 'Zulu', '.': 'Dot' , '@': 'At'}
+nato_phonetic = {'a': 'Alpha', 'c': 'Charlie', 'b': 'Bravo', 'e': 'Echo', 'd': 'Delta', 'g': 'Golf', 'f': 'Foxtrot', 'i': 'India', 'h': 'Hotel', 'k': 'Kilo', 'j': 'Juliet', 'm': 'Mike', 'l': 'Lima', 'o': 'Oscar', 'n': 'November', 'q': 'Quebec', 'p': 'Papa', 's': 'Sierra', 'r': 'Romeo', 'u': 'Uniform', 't': 'Tango', 'w': 'Whiskey', 'v': 'Victor', 'y': 'Yankee', 'x': 'X-ray', 'z': 'Zulu', '.': 'Dot' , '@': 'At', ' ': ' ', '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five', '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine', '0': 'zero'}
 
 if len(sys.argv) < 2:
   print "usage: spell <word1> <word2> <word3>...\nwrites phonetic spelling"
+  print nato_phonetic
 
 for w in sys.argv[1:]:
   try:
